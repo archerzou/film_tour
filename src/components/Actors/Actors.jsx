67ed93f004b5950136501dc2f3ff8ebc35b1d3b9
archerzou <archer.zou84@gmail.com>
2023-01-10 20:08:20 +0800

    complete movie trailer

--- src/components/Actors/Actors.jsx
+++ src/components/Actors/Actors.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+// use useParams to get the actor's id
+// make a new call using redux toolkit query -> get actor details call
+// research tmdb api docs ...
+// use newly created useActorHook to get actor's info to the component
+
 const Actors = () => {
   console.log('Actors');
   return (
--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -36,7 +36,17 @@ export const tmdbApi = createApi({
     getMovie: builder.query({
       query: (id) => `/movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`,
     }),
+
+    //* Get User Specific Lists
+    getRecommendations: builder.query({
+      query: ({ movie_id, list }) => `/movie/${movie_id}/${list}?api_key=${tmdbApiKey}`,
+    }),
   }),
 });
 
-export const { useGetMoviesQuery, useGetGenresQuery, useGetMovieQuery } = tmdbApi;
+export const {
+  useGetMoviesQuery,
+  useGetGenresQuery,
+  useGetMovieQuery,
+  useGetRecommendationsQuery,
+} = tmdbApi;
