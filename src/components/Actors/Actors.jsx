3796a91712fe507e1deb5f05cbf7c5d49c43aab3
archerzou <archer.zou84@gmail.com>
2023-01-12 00:04:26 +0800

    add dependencies

--- src/components/Actors/Actors.jsx
+++ src/components/Actors/Actors.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-
+import { Box, Button, CircularProgress, Grid, Typography } from '@mui/material';
+import { useHistory, useParams } from 'react-router-dom';
+import { ArrowBack } from '@mui/icons-material';
 // use useParams to get the actor's id
 // make a new call using redux toolkit query -> get actor details call
 // research tmdb api docs ...
