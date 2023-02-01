fc168608f917debdf22cd29a78a7f0437b105780
archerzou <archer.zou84@gmail.com>
2023-02-01 19:43:26 +0800

    add alan ai asssistant

---
+++ src/components/FeaturedMovie/FeaturedMovie.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Box, Typography, Card, CardContent, CardMedia } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+const FeaturedMovie = () => (
+  <div>FeaturedMovie</div>
+);
+
+export default FeaturedMovie;
